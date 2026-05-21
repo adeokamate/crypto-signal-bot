cc49f2dbf53927dda1be541e32573cba1857dfc3
Kamate <deotechnolohiesl@gmail.com>
2026-05-21 08:12:42 +0300

    Add SMA trend confirmation and signal logging

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import ccxt
 import pandas as pd
 from ta.momentum import RSIIndicator
+from ta.trend import SMAIndicator
+import logging
+import time
 
+logging.basicConfig(
+    filename="logs/signals.log",
+    level=logging.INFO,
+    format="%(asctime)s | %(message)s"
+)
 
 def fetch_candle_data(symbol="BTC/USDT", timeframe="1h", limit=100):
     exchange = ccxt.binance()
@@ -23,39 +31,76 @@ def calculate_rsi(df, period=14):
     df["rsi"] = rsi.rsi()
     return df
 
+def calculate_sma(df):
+
+    sma20 = SMAIndicator(close=df["close"], window=20)
+    sma50 = SMAIndicator(close=df["close"], window=50)
+
+    df["sma20"] = sma20.sma_indicator()
+    df["sma50"] = sma50.sma_indicator()
+
+    return df
+
 
 def generate_signal(df):
     latest = df.iloc[-1]
 
     price = latest["close"]
     rsi = latest["rsi"]
+    sma20 = latest["sma20"]
+    sma50 = latest["sma50"]
 
-    if rsi < 30:
-        signal = "BUY"
-        reason = "RSI is below 30, market may be oversold"
-    elif rsi > 70:
-        signal = "SELL"
-        reason = "RSI is above 70, market may be overbought"
+    if rsi < 30 and sma20 > sma50:
+        signal = "STRONG BUY"
+        reason = "RSI is oversold and SMA20 is above SMA50, showing bullish trend confirmation"
+
+    elif rsi > 70 and sma20 < sma50:
+        signal = "STRONG SELL"
+        reason = "RSI is overbought and SMA20 is below SMA50, showing bearish trend confirmation"
+
+    elif sma20 > sma50:
+        signal = "HOLD"
+        reason = "Trend is bullish, but RSI has not reached a strong buy zone"
+
+    elif sma20 < sma50:
+        signal = "HOLD"
+        reason = "Trend is bearish, but RSI has not reached a strong sell zone"
+
     else:
         signal = "HOLD"
-        reason = "RSI is neutral"
+        reason = "No clear signal"
 
-    return price, rsi, signal, reason
+    return price, rsi, sma20, sma50, signal, reason
+
+
 
 
 def main():
     df = fetch_candle_data()
     df = calculate_rsi(df)
+    df = calculate_sma(df)
 
-    price, rsi, signal, reason = generate_signal(df)
+    price, rsi, sma20, sma50, signal, reason = generate_signal(df)
 
     print("===== CRYPTO SIGNAL BOT =====")
     print(f"Pair: BTC/USDT")
     print(f"Latest Price: {price}")
     print(f"RSI: {round(rsi, 2)}")
+    print(f"SMA20: {round(sma20, 2)}")
+    print(f"SMA50: {round(sma50, 2)}")
     print(f"Signal: {signal}")
     print(f"Reason: {reason}")
 
 
+    logging.info(
+    f"BTC/USDT | Price: {price} | RSI: {round(rsi,2)} | "
+    f"SMA20: {round(sma20,2)} | SMA50: {round(sma50,2)} | "
+    f"Signal: {signal}"
+)
+
+
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
+        print("\nWaiting 60 seconds before next check...\n")
+        time.sleep(60)
